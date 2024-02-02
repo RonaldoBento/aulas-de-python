42d66354907ec1a24bfae59d5271dac3d51321e1
RonaldoBento <talesmileto295@gmail.com>
2024-02-01 21:24:44 -0300

    novo
    atualizando a lista de exercícios

---
+++ exercicios/testando modulos/teste.py
@@ -0,0 +1,30 @@
+# Enunciado: testando Módulos
+# Programa Desenvolvido em Python 03 
+
+print()
+print('-='*40)
+print(f'\033[1;31m{"Testando Módulos":^80}\033[m')
+print('-='*40)
+print()
+
+from unittest import TestCase, main
+
+from xpto import eh_par
+
+class Testes(TestCase):
+    def test_par(self):
+        self.assertEqual(eh_par(2), True)
+
+    def test_impar(self):
+        self.assertEqual(eh_par(3), False)
+
+    def test_string(self):
+        self.assertEqual(eh_par('string'), False)
+
+    def test_float(self):
+        self.assertEqual(eh_par(4.5), False)
+
+if __name__ == '__main__':
+    main()
+
+   
---
+++ exercicios/testando modulos/xpto.py
@@ -0,0 +1,12 @@
+def eh_par(val: int) -> bool:
+    """
+    Função que verifica se o número é par.
+
+    Arg:
+        - val: Valor de entrada do tipo inteiro
+    """
+    if isinstance(val, int) or isinstance(val, float):
+        return True if val % 2 == 0 else False
+    else:
+        return False
+
