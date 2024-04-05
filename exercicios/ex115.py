d9172b70ccd712df1f0de3ce13ec5dd118a06f1c
RonaldoBento <talesmileto295@gmail.com>
2024-04-05 09:30:05 -0300

    novo
    atualização da lista de exercícios.

---
+++ exercicios/ex115.py
@@ -0,0 +1,24 @@
+#Programa desenvolvido no Python 03
+'''
+Desenvolva um programa que leia dois dados (base e altura) e mostre a área
+do retângulo.
+'''
+print() 
+print('-='*30)
+print('>>>> Área do Retângulo')
+print('-='*30)
+print()
+
+area = 0
+
+base = float(input(' Informe o valor da base do retângulo: '))
+altura = float(input(' Informe o valor da altura do retângulo: '))
+area = base * altura
+print()
+print('-='*30)
+print()
+print(f' A área do retângulo de base {base} e altura {altura} vale {area}.')
+print()
+print(' >>>> Programa desenvolvido com sucesso.')
+print()
+
